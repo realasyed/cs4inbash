065e1695b0e42585d0e7398c4e7f7a6b4d608ae0
Armaan <armaansyed120@gmail.com>
2025-05-20 13:16:59 -0500

    Started work on 'function ascending'. It does not work.

--- a1_basics.sh
+++ a1_basics.sh
@@ -18,6 +18,7 @@
 
 # I know this is inefficient but I wanted to use several Bash features.
 function differ_by_one {
+    echo "Sees if two numbers differ by one."
 
     local result=$((${1}-${2}))
 
@@ -37,6 +38,8 @@ function differ_by_one {
 
 function better_differ_by_one {
 
+    echo "An improved version of 'differ_by_one'."
+
     local difference=$((${1} - ${2}))
 
     if [[ ${difference} == "1" || ${difference} == "-1" ]]; then
@@ -44,3 +47,27 @@ function better_differ_by_one {
         echo "false"
     fi
 } 
+
+# Note on `function`: I prefer the `function` command over the `()` notation
+# because the `()` does not actually take arguments (afaik). In my view,
+# this makes `function` a more honest approach to defining functions in Bash.
+
+# In this next problem, I had to take three inputs and return true if and only
+# if the inputs were in ascending order. In Python this takes two lines of code:
+# def Ascending(x, y, z):
+#   return x<y<z
+
+function ascending {
+
+    echo "Returns 'true' if provided positional arguments are in ascending order."
+
+    # To make things more similar to the Python program.
+    local x=${1}
+    local y=${2}
+    local z=${3}
+
+    if [[ $((x < y < z)) -eq 1 ]]; then # This does not work. Find out why.
+        echo "true"; else
+        echo "false"
+    fi
+}
